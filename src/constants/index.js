11831f12c19aedabf95d546616a521d9d97d817a
Adithya <adithyasn@adsgency.ai>
2026-02-26 12:38:02 -0800

    Update

--- src/constants/index.js
+++ src/constants/index.js
@@ -178,8 +178,11 @@ const experiences = [
     // iconBg: "white",
     date: "Feb 2025 - Present",
     points: [
-      "Architected and deployed a scalable RAG-based AI system using ChromaDB and LLMs to retrieve and index patient records, improving contextual query accuracy and enabling domain-specific reasoning.",
-      "Designed and published a Model Context Protocol (MCP) server enabling agent-to-agent (A2A) communication between LLM clients and external APIs, supporting tool invocation and real-time data retrieval. [[LINK](https://pypi.org/project/nasa-mcp-server/)]",
+      // "Architected and deployed a scalable RAG-based AI system using ChromaDB and LLMs to retrieve and index patient records, improving contextual query accuracy and enabling domain-specific reasoning.",
+      // "Designed and published a Model Context Protocol (MCP) server enabling agent-to-agent (A2A) communication between LLM clients and external APIs, supporting tool invocation and real-time data retrieval. [[LINK](https://pypi.org/project/nasa-mcp-server/)]",
+      // "Building Pulse an Android video editing application using React Native. Implemented a secure end-to-end OAuth 2.0 authentication system with PKCE, JWT token lifecycle, and deep-link based mobile login. [[LINK](https://play.google.com/store/apps/details?id=com.mieweb.pulse)]"
+      "Integrating a RAG pipeline in Ozwell.ai to retrieve patient data from webchartnow.com, index it in ChromaDB, and aiming to boost query accuracy by 50% with patient-specific insights.",
+      "Published a custom Model Context Protocol (MCP) server on PyPI to enable LLM-based clients to access and communicate with NASA open-source APIs, retrieve mission data, and present real-time results to users.[[LINK](https://pypi.org/project/nasa-mcp-server/)]",
       "Building Pulse an Android video editing application using React Native. Implemented a secure end-to-end OAuth 2.0 authentication system with PKCE, JWT token lifecycle, and deep-link based mobile login. [[LINK](https://play.google.com/store/apps/details?id=com.mieweb.pulse)]"
     ],
   },
